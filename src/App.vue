f60524d273d67043ceb83d7b051bd11f9bbaab83
husend <1175674846@qq.com>
2020-01-17 17:11:18 +0800

    上传项目

--- src/App.vue
+++ src/App.vue
@@ -32,13 +32,9 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
 </style>
